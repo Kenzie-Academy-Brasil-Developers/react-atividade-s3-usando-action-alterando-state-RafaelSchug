355d7ff273b2fee5e3ee4547c6ff2ddb7b867ad0
Rafael Schug <rafaelschugbc@gmail.com>
2021-09-28 13:36:11 -0300

    fix: changes in Provider and store import, from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import {Provider} from 'react-redux';
-import store from './store';
 import UserCard from './component/UserCard';
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
+    <div className="App">
         <UserCard></UserCard>
      </div>
-    </Provider>
   );
 }
 
